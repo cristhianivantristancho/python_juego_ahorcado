d825c51457aee2e7ce3848302a84de47c1ef6c91
cristhianivantristancho <cristhianivantristancho@gmail.com>
2024-12-25 21:39:18 -0500

    Realizamos un codigo de python para el juego ahorcado

---
+++ juego_del_ahorcado.py
@@ -0,0 +1,72 @@
+import random
+
+
+def obtener_palabra_secreta() -> str:
+    palabras = [
+        "palabra",
+        "javascript",
+        "java",
+        "angular",
+        "django",
+        "tensorflow",
+        "react",
+        "typescript",
+        "git",
+        "flask",
+    ]
+    return random.choice(palabras)
+
+
+def mostrar_progreso(palabra_secreta, letras_adivinadas):
+    adivinado = ""
+
+    for letra in palabra_secreta:
+        if letra in letras_adivinadas:
+            adivinado += letra
+        else:
+            adivinado += "_"
+
+    return adivinado
+
+
+def juego_ahorcado():
+    palabra_secreta = obtener_palabra_secreta()
+    letras_adivinadas = []
+    intentos = 7
+    juego_terminado = False
+
+    print("Bienvenido al juego del Ahorcado")
+    print(f"Tienes {intentos} intentos para adivinar la palabra secreta")
+    print(mostrar_progreso(palabra_secreta, letras_adivinadas),'la cantidad de letras de la palabra es:', len(palabra_secreta))
+
+    while not juego_terminado and intentos > 0:
+        adivinanza = input("Introduce una letra: ").lower()
+
+        if len(adivinanza) != 1 or not adivinanza.isalpha():
+            print("Por favor introduzca una letra valida (Solo escribir una letra)")
+        elif adivinanza in letras_adivinadas:
+            print("Ya has utilizado esa letra, prueba con otra")
+        else:
+            letras_adivinadas.append(adivinanza)
+
+            if adivinanza in palabra_secreta:
+                print(f"Muy bien has acertado, la letra '{adivinanza}' esta en la palabra")
+            else:
+                intentos -= 1
+                print(
+                    f"Lo siento mucho, la letra '{adivinanza}' no esta presente en la palabra secreta"
+                )
+                print(f"Te quedan {intentos} intentos")
+
+        progreso_actual= mostrar_progreso(palabra_secreta,letras_adivinadas)
+        print(progreso_actual)
+        
+        if '_' not in progreso_actual:
+            juego_terminado=True
+            palabra_secreta=palabra_secreta.capitalize()
+            print(f"Felicitaicones has ganado, La palabra completa es: '{palabra_secreta}'")
+            
+    if intentos == 0:
+        print(f"Lo sentimos mucho se te han acabado los intentos, la palabra entera es: '{palabra_secreta}'")
+
+juego_ahorcado()
